fdc05f8a1dc4960e81f0ffba84f4124b2932b55f
theeluwin <theeluwin@gmail.com>
2017-01-02 07:36:36 +0900

    adding karger min cut

--- algorithms/Karger Min Cut/kmc.py
+++ algorithms/Karger Min Cut/kmc.py
@@ -1,1 +1,54 @@
 # -*- coding: utf-8 -*-
+
+import time
+import random
+
+random.seed(time.time())
+
+
+class Edge(object):
+
+    def __init__(self, vi, vt, index=0):
+        # the index and o-prefixeds are just incase when you want to back-track which edges should be deleted from the origin
+        self.index = index
+        self.ovi = vi
+        self.ovt = vt
+        self.vi = vi
+        self.vt = vt
+
+
+class Graph(object):
+
+    def __init__(self, alists=[]):
+        edge_index = 1
+        self.n = len(alists)
+        self.alists = [None] + alists
+        self.edges = []
+        for i in range(1, self.n + 1):
+            for j in self.alists[i]:
+                if j > i:
+                    self.edges.append(Edge(i, j, edge_index))
+                    edge_index += 1
+
+    def karger_min_cut(self):
+        while self.n > 2:
+            index = random.randrange(0, len(self.edges))
+            edge = self.edges[index]
+            vi = edge.vi
+            vt = edge.vt
+            self.edges = self.edges[:index] + self.edges[index + 1:]
+            self.contract(vi, vt)
+            self.n = self.n - 1
+        return len(self.edges)
+
+    def contract(self, nvi, nvt):
+        for edge in self.edges:
+            if edge.vi == nvi:
+                edge.vi = nvt
+            if edge.vt == nvi:
+                edge.vt = nvt
+        edges = []
+        for edge in self.edges:
+            if edge.vi != edge.vt:
+                edges.append(edge)
+        self.edges = edges
--- algorithms/Karger Min Cut/tests.py
+++ algorithms/Karger Min Cut/tests.py
@@ -1,13 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import kmc
+import math
+import codecs
 import unittest
 
 
 class TestKargerMinCut(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.alists = [[2, 3], [1, 3], [1, 2]]
+
+    def reader(self):
+        file = codecs.open('data.txt', 'r', encoding='utf-8')
+        alists = [[int(v) for v in line.strip().split('\t')[1:]] for line in file]
+        file.close()
+        return alists
+
+    def test_init(self):
+        graph = kmc.Graph(self.alists)
+        self.assertEqual(3, len(graph.edges))
+
+    def test_graph_init(self):
+        graph = kmc.Graph(self.alists)
+        graph.contract(1, 2)
+        self.assertEqual(2, len(graph.edges))
+
+    def test_karger_min_cut(self):
+        graph = kmc.Graph(self.alists)
+        k = graph.karger_min_cut()
+        self.assertEqual(2, k)
+
+    def test_coursera(self):
+        n = 200
+        # T = n * n * int(math.ceil(math.log(n)))  # this guarantees the probability that the algorithm fails to be lower than 1/n, but my algorithm was too slow! I wasted time on contracting edges by O(m) per iteration...
+        T = n  # but hey, this gave me correct answer
+        ks = []
+        alists = self.reader()
+        for t in range(T):
+            graph = kmc.Graph(alists)
+            ks.append(graph.karger_min_cut())
+        # self.assertEqual(17, min(ks))
+        # actually, since this is a random algorithm, testing whether the output equals to the answer is just just just wrong thing to do! you should test whether the graph is splitted by the output edges. never ever do it like this. this is just terribly wrong.
+        print(min(ks))
 
 
 if __name__ == '__main__':
